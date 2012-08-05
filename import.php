1b096100e5ba6bf0c7262e673a86df3f70dcf8d1
Floris Lambrechts <florisla@gmail.com>
2012-08-05 14:32:49 +0200

    Improve configurability and code comments of the import script

--- import.php
+++ import.php
@@ -1,7 +1,13 @@
 <?php
 
 // put this file in the Drupal root dir
-// OR: chdir('/home/org/vitis/www/www.vitis-tct.be/');
+// OR add a line of PHP to chdir into it: chdir('/path/to/site');
+
+// configuration:
+$highest_article_number = 105;
+$drupal_node_content_type = 'page';
+$drupal_author_user_id = 3;
+
 // configure drupal root dir
 define('DRUPAL_ROOT', getcwd());
 
@@ -10,24 +16,23 @@ require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL); 
 
 // process all files
-$max_number = 104;
-
-for ($i=0; $i<$max_number; $i++)
+for ($i=0; $i<=$highest_article_number; $i++)
 {
-    $url = file_get_contents(sprintf("./imports/article%02d.url.txt", $i));
-    $title = file_get_contents(sprintf("./imports/article%02d.title.txt", $i));
-    $body = file_get_contents(sprintf("./imports/article%02d.body.html", $i));
+    // read all three files
+    $url = file_get_contents(sprintf("./imports/article%05d.url.txt", $i));
+    $title = file_get_contents(sprintf("./imports/article%05d.title.txt", $i));
+    $body = file_get_contents(sprintf("./imports/article%05d.body.html", $i));
     echo($url . "<br/>");
 
-    // create node of the desired type
+    // create a new Drupal node (of the desired content type)
     $node = new stdClass();
-    $node->type = 'page';
+    $node->type = $drupal_node_content_type;
     node_object_prepare($node);
 
     // set basic data
     $node->title    = $title;
     $node->language = LANGUAGE_NONE;
-    $node->uid = 3;
+    $node->uid = $drupal_author_user_id;
 
     // set content
     $node->body[$node->language][0]['value']   = $body;
