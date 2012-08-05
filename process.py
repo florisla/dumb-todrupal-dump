c12121d303463c8ce7862cf6f215984e4e921313
Floris Lambrechts <florisla@gmail.com>
2012-08-05 14:28:20 +0200

    Improve configurability and add more code comments

--- process.py
+++ process.py
@@ -1,38 +1,63 @@
 #!/usr/bin/python3
 
+"""Simple script to write title, url, and HTML body content of HTML files
+to separate numberd files.
+
+Allows to do custom replacements.
+
+Note on security: we assume that your input files are correctly HTML-encoded,
+and contain no harmful code.
+If your input data is different, then take precautions!
+"""
+
 import glob
 import os
 import re
 
+# make sure that these folders exist
+static_dump_dir = '~/site-dump'
+processed_dir = '~/site-dump/processed'
+
+title_prefix_to_remove = 'Site: Main - '
+html_replacements = {
+    '/site/' : '/',
+    '/HomePage/' : '/HomePage'
+}
+
+# regular expression to parse out the title of the page
 TITLE = re.compile(".*<title>(.*)</title>")
+# regular expression to parse out the content HTML of the page
 BODY = re.compile("<!--PageText-->(.*)<div id=\"footer\"", re.DOTALL)
 
-def process(f, index):
+def process(index, file):
+    """Parse out title, filename, body and do necessary replacements"""
     with open(f, 'r', encoding='iso-8859-1') as file:
         content = file.read()
-        m = TITLE.search(content)
-        if m:
-            title = m.groups()[0].replace('Vitis-tct : Main - ', '')
+        match = TITLE.search(content)
+        if match:
+            title = match.groups()[0]
+            title = title.replace(title_prefix_to_remove, '')
             filename = os.path.basename(f).replace('.html', '')
-            print(filename, ':')
-            print(title)
-        m2 = BODY.search(content)
-        if m2:
-            body = m2.groups()[0]
-            body = body.replace('/site/Main/', '/').replace('/HomePage/', '/HomePage')
+            print(filename, ':', title)
+        match2 = BODY.search(content)
+        if match2:
+            body = match2.groups()[0]
+            for old,new in html_replacements.items():
+                body = body.replace(old, new)
             print(body)
-    output_filename = "article{0:02}".format(index)
+
+    # write body, title and URL to separate numbered files
+    output_filename = "article{0:05}".format(index)
     with open(output_filename + '.body.html', 'tw', encoding='utf-8') as body_out:
         body_out.write(body)
     with open(output_filename + '.title.txt', 'tw', encoding='utf-8') as title_out:
         title_out.write(title)
     with open(output_filename + '.url.txt', 'tw', encoding='utf-8') as url_out:
         url_out.write(filename)
-   
-os.chdir('/home/flo/backup/www.vitis-tct.be/converted')
-files = [f for f in glob.iglob("/home/flo/backup/www.vitis-tct.be/site/Main/*") if os.path.isfile(f)]
-for i,f in enumerate(files):
-    process(f, i)
 
+if __name__ == '__main__':
+    os.chdir(processed_dir)
+    for i,file in enumerate(f for f in glob.iglob(static_dump_dir + "/*") if os.path.isfile(f))
+        process(i, file)
 
 
